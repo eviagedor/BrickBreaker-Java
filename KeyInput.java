5a0e8a554105ea724694e7efc20a23558f0a6753
Edmund Viagedor <ed.viagedor204@gmail.com>
2018-05-15 02:41:01 -0500

    Removed check for if object has ID of paddle since only used by paddle

--- KeyInput.java
+++ KeyInput.java
@@ -12,23 +12,18 @@ public class KeyInput extends KeyAdapter {
 	}
 
 	public void keyPressed(KeyEvent e) {
-		if (object.getId() == ObjectID.PADDLE) {
-			if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-				object.setVelocityX(5);
-			} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-				object.setVelocityX(-5);
-			}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			object.setVelocityX(5);
+		} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			object.setVelocityX(-5);
 		}
 	}
 
 	public void keyReleased(KeyEvent e) {
-		if (object.getId() == ObjectID.PADDLE) {
-			if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-				object.setVelocityX(0);
-			} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-				object.setVelocityX(0);
-			}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			object.setVelocityX(0);
+		} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			object.setVelocityX(0);
 		}
 	}
-
 }
