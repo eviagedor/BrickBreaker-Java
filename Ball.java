3291c866a1c593f35fb738026cfb937f0b1e3e8b
Edmund Viagedor <ed.viagedor204@gmail.com>
2018-05-15 02:25:42 -0500

    Detects collision with paddle & stops moving if passed paddle

--- Ball.java
+++ Ball.java
@@ -2,31 +2,49 @@ package game;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 public class Ball extends GameObject {
 
-	public Ball(int x, int y) {
+	ObjectHandler handler;
+	
+	public Ball(int x, int y, ObjectHandler handler) {
 		super(ObjectID.BALL, x, y);
+		this.handler = handler;
 		setVelocityX(3);
 		setVelocityY(3);
+	}
+	
+	private void collisionDetection() {
+		GameObject paddle = handler.getObject(0);
+		
+		if(getBounds().intersects(paddle.getBounds())) {
+			velocityX *= -1;
+			velocityY *= -1;
+		}
 	}
 
 	@Override
 	public void tick() {
 		x += velocityX;
 		y += velocityY;
-
-		if (x <= 0 || x >= 325 - 32) {
+		
+		if (x <= 0 || x >= Game.WIDTH - 32) {
 			velocityX *= -1; // reverse the direction
+			//System.out.println("Hit left or right");
 		}
 
-		if (y <= 0 || y >= 500 - 64) {
+		if (y <= 0) {
 			velocityY *= -1; // reverse the direction
-
-			// System.out.println("Game over!");
-		} /*
-			 * else if (y <= 0) { velocityY *= -1; }
-			 */
+			//System.out.println("Hit top");
+		} 
+		
+		if(y >= Game.HEIGHT) {
+			velocityX = 0;
+			velocityY = 0;
+		} 
+		// did the ball hit the paddle?
+		collisionDetection();
 	}
 
 	@Override
@@ -35,4 +53,8 @@ public class Ball extends GameObject {
 		g.fillOval(x, y, 16, 16);
 	}
 
+	@Override
+	public Rectangle getBounds() {
+		return new Rectangle(x, y, 16, 16);
+	}
 }
