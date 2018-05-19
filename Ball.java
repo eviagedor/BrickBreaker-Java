6cdefb593bd709f3f771ccec40fe8a55f211889e
Edmund Viagedor <ed.viagedor204@gmail.com>
2018-05-19 04:02:32 -0500

    Removed collision detection and moved to Game class

--- Ball.java
+++ Ball.java
@@ -6,45 +6,16 @@ import java.awt.Rectangle;
 
 public class Ball extends GameObject {
 
-	ObjectHandler handler;
-	
-	public Ball(int x, int y, ObjectHandler handler) {
+	public Ball(int x, int y) {
 		super(ObjectID.BALL, x, y);
-		this.handler = handler;
 		setVelocityX(3);
 		setVelocityY(3);
-	}
-	
-	private void collisionDetection() {
-		GameObject paddle = handler.getObject(0);
-		
-		if(getBounds().intersects(paddle.getBounds())) {
-			velocityX *= -1;
-			velocityY *= -1;
-		}
 	}
 
 	@Override
 	public void tick() {
 		x += velocityX;
 		y += velocityY;
-		
-		if (x <= 0 || x >= Game.WIDTH - 32) {
-			velocityX *= -1; // reverse the direction
-			//System.out.println("Hit left or right");
-		}
-
-		if (y <= 0) {
-			velocityY *= -1; // reverse the direction
-			//System.out.println("Hit top");
-		} 
-		
-		if(y >= Game.HEIGHT) {
-			velocityX = 0;
-			velocityY = 0;
-		} 
-		// did the ball hit the paddle?
-		collisionDetection();
 	}
 
 	@Override
